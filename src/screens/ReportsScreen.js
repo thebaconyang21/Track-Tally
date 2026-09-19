b78e3dd5545d81c05de3501d53e69a9351834209
thebaconyang21 <beahpayot21@gmail.com>
2026-09-19 23:15:17 +0800

    Yeah

--- src/screens/ReportsScreen.js
+++ src/screens/ReportsScreen.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+// import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import {
   getDailySalesSummary,
